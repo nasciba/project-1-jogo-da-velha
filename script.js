05fc1d088786e69d46a5a981e5d7425cd8ee3d14
nasciba <amandanasciba@gmail.com>
2019-10-06 19:32:17 -0300

    Table with game grid created

--- script.js
+++ script.js
@@ -54,15 +54,24 @@ function startGame() {
     const telaInicial = document.getElementById("tela-inicial");
     const telaJogo = document.getElementById("tela-jogo");
     if (name1 === '' || name2 === '') {
-        return alert("Por favor, insira os nomes dos jogadores para continuar");
+        alert("Por favor, insira os nomes dos jogadores para continuar");
         
     }
     else {
-        
-        return telaInicial.classList.add("hidden-section"), telaJogo.classList.remove("hidden-section");
+        telaInicial.classList.add("hidden-section"), telaJogo.classList.remove("hidden-section");
     }
 }
 
 const startButton = document.getElementById("start-game")
-startButton.addEventListener("click", startGame)
+startButton.addEventListener("click", startGame);
+
+const square1 = document.getElementById("square1");
+const square2 = document.getElementById("square2");
+const square3 = document.getElementById("square3");
+const square4 = document.getElementById("square4");
+const square5 = document.getElementById("square5");
+const square6 = document.getElementById("square6");
+const square7 = document.getElementById("square7");
+const square8 = document.getElementById("square8");
+const square9 = document.getElementById("square9");
 
